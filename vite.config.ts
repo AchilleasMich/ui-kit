2b512c86edf1eedc02d86931a5e8fa037183cf1c
AchilleasMich <achmichael@gmail.com>
2025-06-29 17:33:06 +0300

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "UiKit",
+      formats: ["es", "cjs"],
+      fileName: (format) => `my-ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
