1dd576a9d6edef6efa0fac8eb4eeff724580e5f3
AchilleasMich <achmichael@gmail.com>
2025-07-10 16:30:32 +0300

    Yet another try for css

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
+    cssInjectedByJsPlugin(),
     dts({
       insertTypesEntry: true,
       outDir: "dist",
