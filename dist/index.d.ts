a51a5e05296d89e03f98988a52e0fc39d32cad2d
AchilleasMich <achmichael@gmail.com>
2025-07-10 15:56:58 +0300

    Fixes for build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,20 +1,25 @@
-import { ClassProp } from 'class-variance-authority/types';
-import { default as default_2 } from 'react';
-import { VariantProps } from 'class-variance-authority';
+import { ClassProp } from "class-variance-authority/types";
+import React from "react";
+import { VariantProps } from "class-variance-authority";
 
-export declare const Button: default_2.FC<ButtonProps>;
+export declare const Button: React.FC<ButtonProps>;
 
-export declare interface ButtonProps extends default_2.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
-}
+export declare interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
-declare const buttonVariants: (props?: ({
-    variant?: "primary" | "secondary" | "tertiary" | null | undefined;
-    size?: "xs" | "sm" | "md" | "lg" | "xl" | null | undefined;
-    rounded?: "sm" | "md" | "lg" | "none" | "full" | null | undefined;
-    shadow?: "sm" | "md" | "lg" | "none" | null | undefined;
-    width?: "full" | "auto" | "fit" | null | undefined;
-    icon?: "none" | "left" | "right" | "only" | null | undefined;
-    uppercase?: boolean | null | undefined;
-} & ClassProp) | undefined) => string;
+declare const buttonVariants: (
+  props?:
+    | ({
+        variant?: "primary" | "secondary" | "tertiary" | null | undefined;
+        size?: "xs" | "sm" | "md" | "lg" | "xl" | null | undefined;
+        rounded?: "sm" | "md" | "lg" | "none" | "full" | null | undefined;
+        shadow?: "sm" | "md" | "lg" | "none" | null | undefined;
+        width?: "full" | "auto" | "fit" | null | undefined;
+        icon?: "none" | "left" | "right" | "only" | null | undefined;
+        uppercase?: boolean | null | undefined;
+      } & ClassProp)
+    | undefined,
+) => string;
 
-export { }
+export {};
